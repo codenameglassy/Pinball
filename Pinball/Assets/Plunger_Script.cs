eb0b3f2a9ebf42c5df53f9d29a2e52be672fc533
codenameglassy <codenameglassy@gmail.com>
2022-03-22 11:59:13 +0545

    Plunger

--- Pinball/Assets/Plunger_Script.cs
+++ Pinball/Assets/Plunger_Script.cs
@@ -1,18 +1,82 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Plunger_Script : MonoBehaviour
 {
+
+    float power;
+    [SerializeField] private float addPower;
+    [SerializeField] private float maxPower;
+    [SerializeField] private Slider powerSlider;
+
+
+    List<Rigidbody> ballList;
+    bool ballReady;
     // Start is called before the first frame update
     void Start()
     {
-        
+        powerSlider.minValue = 0f;
+        powerSlider.maxValue = maxPower;
+        ballList = new List<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (ballReady)
+        {
+            powerSlider.gameObject.SetActive(true);
+        }
+        else
+        {
+            powerSlider.gameObject.SetActive(false);
+        }
+
+        powerSlider.value = power;
+
+        if(ballList.Count > 0)
+        {
+            ballReady = true;
+            if (Input.GetKey(KeyCode.Space))
+            {
+                if(power <= maxPower)
+                {
+                    power += addPower * Time.deltaTime;
+                }
+            }
+
+            if (Input.GetKeyUp(KeyCode.Space))
+            {
+                foreach (Rigidbody r in ballList)
+                {
+                    r.AddForce(power * -Vector3.forward);
+                }
+            }
+        }
+        else
+        {
+            ballReady = false;
+            power = 0f;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ball"))
+        {
+            ballList.Add(other.gameObject.GetComponent<Rigidbody>());
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Ball"))
+        {
+            ballList.Remove(other.gameObject.GetComponent<Rigidbody>());
+            power = 0f;
+        }
     }
 }
